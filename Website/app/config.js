810cf9135fb6c9d77594e318f618d74b52f174c2
Kang Cheng <me@chengkang.pw>
2016-07-12 21:32:34 +0800

    some init things

---
+++ Website/app/config.js
@@ -0,0 +1,6 @@
+// 请将 CONFIG_UID 修改为 config 页面显示的 uid
+// 例：var CONFIG_UID = "github:8396515";
+var CONFIG_UID = "github:8396515";
+//请将 CONFIG_DUOSHUO 修改为你注册的多说 id
+// 例：var CONFIG_DUOSHUO = "chengkang";
+var CONFIG_DUOSHUO = "chengkang";
